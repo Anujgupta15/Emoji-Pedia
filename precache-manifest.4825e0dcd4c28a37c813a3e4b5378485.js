744152411c113afb6cd747db6a153fde8d5d2237
Anuj Gupta <anujgupt15@gmail.com>
2021-10-28 23:25:52 +0530

    Updates

---
+++ precache-manifest.4825e0dcd4c28a37c813a3e4b5378485.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826261ce1a6ad7ac061d28cc2e52522e",
+    "url": "/Emoji-Pedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/Emoji-Pedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "653f73d14d3745240ff8",
+    "url": "/Emoji-Pedia/static/js/main.a2469041.chunk.js"
+  },
+  {
+    "revision": "9cb35f0aacf828c33064",
+    "url": "/Emoji-Pedia/static/js/runtime-main.3fd93624.js"
+  }
+]);
